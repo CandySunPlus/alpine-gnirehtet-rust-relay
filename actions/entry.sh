fe09c796f1ea326c25b13212d498303a327a3360
Fengming Sun <sunfengming@meituan.com>
2021-06-19 02:33:30 +0800

    add pub key

--- actions/entry.sh
+++ actions/entry.sh
@@ -4,11 +4,20 @@ set -e
 export HOME=/home/builder
 
 main() {
-  sudo chown -R builder:abuild .
+  SRC=$HOME/src
+  ABUILD=$HOME/.abuild
+
   sudo mkdir $INPUT_OUTPUT
   sudo chown -R builder:abuild $INPUT_OUTPUT
 
-  abuild-keygen -i -a -n
+  ["$ID_RSA"] && {
+    echo -e "$ID_RSA" > $ABUILD/id_rsa
+    export PACKAGE_PRIVKEY=$ABUILD/id_rsa
+  }
+
+  mkdir $ABUILD $SRC
+  cp -r ./* $SRC && cd $SRC
+
   exec abuild -r -P $INPUT_OUTPUT
 }
 
