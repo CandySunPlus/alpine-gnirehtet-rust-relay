a726c6caf65550e0cebd31768b30411df02a678b
Fengming Sun <sunfengming@meituan.com>
2021-06-19 02:08:19 +0800

    fix

--- actions/entry.sh
+++ actions/entry.sh
@@ -3,8 +3,7 @@
 export HOME=/home/builder
 
 sudo mkdir $INPUT_OUTPUT && sudo chown builder:abuild $INPUT_OUTPUT
-cp -r $GITHUB_WORKSPACE/* $HOME
-cd $HOME
+sudo mkdir src && sudo chown builder:abuild src
 
 abuild-keygen -i -a -n
 abuild -r -P $INPUT_OUTPUT
