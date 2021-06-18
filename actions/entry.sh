b9b06b66653c993d2ea24e1249ec08592c90034d
Fengming Sun <sunfengming@meituan.com>
2021-06-19 02:13:51 +0800

    fix

--- actions/entry.sh
+++ actions/entry.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+set -e
 export HOME=/home/builder
 
-sudo mkdir $INPUT_OUTPUT && sudo chown builder:abuild $INPUT_OUTPUT
-sudo mkdir src && sudo chown builder:abuild src
+main() {
+  sudo chown -R builder:abuild .
+  sudo mkdir $INPUT_OUTPUT
+  sudo chown -R builder:abuild $INPUT_OUTPUT
 
-abuild-keygen -i -a -n
-abuild -r -P $INPUT_OUTPUT
+  abuild-keygen -i -a -n
+  exec abuild -r -P $INPUT_OUTPUT
+}
 
-
+main "$@"
