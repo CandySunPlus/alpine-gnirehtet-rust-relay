ab843ea8edc7e6dfff919b15c9788f6593c4e5d2
Fengming Sun <sunfengming@meituan.com>
2021-06-19 01:37:35 +0800

    fix

--- actions/entry.sh
+++ actions/entry.sh
@@ -2,13 +2,13 @@
 
 export HOME=/home/builder
 
-cd /home/builder
-cp -r /github/workspace/* .
+cd $HOME && mkdir -p packages/home
+cp -r "${GITHUB_WORKSPACE}"/* .
 
 abuild-keygen -i -a -n
 abuild -r
 
-echo "Copy package to output path..."
+echo "Copy package to output path ${INPUT_OUTPUT}"
 sudo mkdir $INPUT_OUTPUT
 sudo cp $HOME/packages/home/**/*.apk $INPUT_OUTPUT
 
